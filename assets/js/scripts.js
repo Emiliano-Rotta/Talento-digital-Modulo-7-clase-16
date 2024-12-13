0c2ec26914ef29f6f4bcb9548a18979bb7d9cc79
Emiliano Rotta <emilianorottaf@gmail.com>
2024-12-13 20:06:55 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+const baseURL = 'https://rickandmortyapi.com/api/character/'
+var nombre = document.getElementById('character-name')
+var imagen = document.getElementById('character-image')
+var estado = document.getElementById('character-status')
+
+async function fetchCaracter() {
+    try {
+        const response = await fetch(`${baseURL}`)
+        const character = await response.json();
+        nombre.textContent = character.name
+        imagen.src = character.image
+        estado.textContent = character.status
+        console.log(character)
+
+    } catch (error) {
+        console.error('Error en el personaje:', error)
+    }
+    
+}
+fetchCaracter()
